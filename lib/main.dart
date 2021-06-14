098e3c98fec27d600658922c8f88e6cf80bdce34
Swaroop <swaroop.sambhayya@gmail.com>
2021-06-14 21:24:54 +0530

    Added app name

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo',
       debugShowCheckedModeBanner: false,
       theme: themeData,
       home: ValueListenableBuilder(
